626a9668d2b8c4d5771965d4e11a19d6bf121263
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2023-02-03 23:21:26 +0100

    Add filesystem_watcher documentation

--- src/fs/filesystem_watcher.hpp
+++ src/fs/filesystem_watcher.hpp
@@ -1,6 +1,30 @@
 
 #pragma once
 
+/* filesystem_watcher.hpp
+ *
+ * watch over directories and files.
+ * the filesystem_watcher takes a callback function and runs in another thread
+ * after starting.
+ *
+ * usage:
+ *
+ *    void callback_function(const char *path, fs::watcher_event_type event)
+ *    { ... }
+ *
+ *    fs::filesystem_watcher *watcher;
+ *    fs::create_filesystem_watcher(&watcher, callback_function);
+ *
+ *    fs::watch_file(watcher, "/etc/shadow");
+ *
+ *    fs::start_filesystem_watcher(watcher);
+ *
+ *    // do other things
+ *
+ *    fs::stop_filesystem_watcher(watcher);
+ *    fs::destroy_filesystem_watcher(watcher);
+ */
+
 #include "shl/number_types.hpp"
 #include "shl/enum_flag.hpp"
 
